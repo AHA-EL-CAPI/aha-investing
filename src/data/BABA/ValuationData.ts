f732a0db421cd6630c5b6f6f7a116aaddaa0b096
Alejandro Hernández Arboleda <a.hernandez105@uniandes.edu.co>
2025-06-21 17:50:08 -0500

    Update price data: 2025-06-21 17:50:08

---
+++ src/data/BABA/ValuationData.ts
@@ -0,0 +1,15 @@
+export const valuationTable = {
+  headers: ['Unidades', 'USD $', 'Bear', 'Basis', 'Bull'],
+  rows: [
+    ["BABA", "GRF5Y\GRL5Y", "15", "18", "21"],
+    ["Bear", "15", 123.26, 135.84, 149.7],
+    ["Basis", "18", 138.93, 153.24, 169.0],
+    ["Bull", "21", 156.21, 172.43, 190.3],
+  ],
+  quantities: {
+    "Initial FCF": 6,
+    "FCF multiple": 20,
+    "Disccount Rate": 15,
+    "Margin of safety": 25,
+  }
+};
