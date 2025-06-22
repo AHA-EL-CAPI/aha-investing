7fedf793f9c53a35b6e02ee00e185c958c80865f
Alejandro Hernández Arboleda <a.hernandez105@uniandes.edu.co>
2025-06-22 17:46:45 -0500

    general: addition of several companies

--- src/scripts/NFLX-price-history.py
+++ src/scripts/NFLX-price-history.py
@@ -4,7 +4,7 @@ import yfinance as yf
 import pandas as pd
 import json
 
-ticker = yf.Ticker("AAPL")
+ticker = yf.Ticker("NFLX")
 data = ticker.history(period="1y")  # You can also use "5y", "1y", etc.
 
 # Reduce data to only date and closing price
@@ -13,7 +13,7 @@ result = [{"date": str(idx.date()), "close": round(row["Close"], 2)} for idx, ro
 # print(json.dumps(result))
 
 # Convert to valid TypeScript export
-with open("src/data/AAPL/price-history.ts", "w", encoding="utf-8") as f:
+with open("src/data/NFLX/price-history.ts", "w", encoding="utf-8") as f:
     f.write("export const priceHistory = ")
     f.write(str(result).replace("'", '"'))  # convert to double quotes for valid TS
     f.write(";\n")
