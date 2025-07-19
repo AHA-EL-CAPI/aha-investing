6df34d676b11398e31c7ae2402088d4e8542a084
Alejandro Hernández Arboleda <a.hernandez105@uniandes.edu.co>
2025-07-19 11:55:15 -0500

    Update price data: 2025-07-19 11:55:15

---
+++ src/data/JPM/ValuationData.ts
@@ -0,0 +1,15 @@
+export const valuationTable = {
+  headers: ['Unidades', 'USD $', 'Bear', 'Basis', 'Bull'],
+  rows: [
+    ["JPM", "GRF5Y\GRL5Y", "8", "12", "16"],
+    ["Bear", "8", 241.44, 267.24, 296.62],
+    ["Basis", "12", 282.15, 313.09, 348.33],
+    ["Bull", "16", 328.58, 365.46, 407.45],
+  ],
+  quantities: {
+    "Initial FCF": 24.0,
+    "FCF multiple": 10,
+    "Disccount Rate": 15,
+    "Margin of safety": 15,
+  }
+};
